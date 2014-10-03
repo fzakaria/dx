ad47c8795a80bc9e012262cfc372efc44127d3db
Farid Zakaria <farid.m.zakaria@gmail.com>
2014-10-03 19:03:19 +0000

    mimicked the project created by scrapy

---
+++ agora/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for dx project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'dx'
+
+SPIDER_MODULES = ['agora.spiders']
+NEWSPIDER_MODULE = 'agora.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'dx (+http://www.yourdomain.com)'
